60e747586645bacafa2d2e52c0c86794fe8870bb
NikolayD <volodar86@yandex.ru>
2024-07-24 22:08:49 +0300

    create class PersonListViewController for second table view controller.

--- ContactList/ViewControllers/ContactListViewController.swift
+++ ContactList/ViewControllers/ContactListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class ContactListViewController: UITableViewController {
     
-    private let personList = Person.getPersonList()
+    var personList: [Person]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
