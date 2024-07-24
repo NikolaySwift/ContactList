4805a607591608191897ae71a8a5efb849710e52
NikolayD <volodar86@yandex.ru>
2024-07-24 20:57:03 +0300

    create outlets for labels and fill them with person phone and email in viewDidLoad.

--- ContactList/ViewControllers/ContactDetailsViewController.swift
+++ ContactList/ViewControllers/ContactDetailsViewController.swift
@@ -7,23 +7,18 @@
 
 import UIKit
 
-class ContactDetailsViewController: UIViewController {
+final class ContactDetailsViewController: UIViewController {
 
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = person.fullName
+        phoneLabel.text = "Phone: \(person.phoneNumber)"
+        emailLabel.text = "Email: \(person.email)"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
