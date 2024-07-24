ee76e2d2168e0eb98f4df623aaa79596a982e51b
NikolayD <volodar86@yandex.ru>
2024-07-24 20:59:25 +0300

    in method prepare send person data to contact details view controller. override methods to setup count of row and to fill cell

--- ContactList/ViewControllers/ContactListViewController.swift
+++ ContactList/ViewControllers/ContactListViewController.swift
@@ -8,11 +8,38 @@
 import UIKit
 
 final class ContactListViewController: UITableViewController {
+    
+    private let personList = Person.getPersonList()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.backButtonTitle = "Back"
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let contactDetailsVC = segue.destination as? ContactDetailsViewController else {
+            return
+        }
+        guard let currentIndex = tableView.indexPathForSelectedRow else { return }
+        contactDetailsVC.person = personList[currentIndex.row]
+    }
 
 }
 
+// MARK: - UITableViewDataSource
+extension ContactListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        personList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = personList[indexPath.row].fullName
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+}
